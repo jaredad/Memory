33196d90b7a28310d9632896a2ccdabcb4f5a769
Austin Jared <35081849+jaredad@users.noreply.github.com>
2019-04-23 01:22:46 -0500

    Touch Door

---
+++ Assets/TouchDoor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TouchDoor : MonoBehaviour
+{
+    public string scene;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(scene);
+        }
+        
+    }
+}
