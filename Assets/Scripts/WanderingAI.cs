078e9c53f7f79633a7627e31a2ce211533968bf9
Blake Hawkins <hawkinsba@hendrix.edu>
2019-04-29 20:36:28 -0500

    Fixed bug in WanderingAI.cs
    Enemies now wander around like they should and begin moving towards the player if they're within a certain distance.

--- Assets/Scripts/WanderingAI.cs
+++ Assets/Scripts/WanderingAI.cs
@@ -7,11 +7,50 @@ public class WanderingAI : MonoBehaviour
 
     public GameObject p;
     public int MoveSpeed = 4;
+    public float wanderRadius;
+    public float wanderTimer;
+    private Transform target;
+    private NavMeshAgent agent;
+    private float timer;
 
+    // Use this for initialization
+    void OnEnable()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        timer = wanderTimer;
+    }
 
+    // Update is called once per frame
     void Update()
     {
-        transform.LookAt(p.transform);
-        transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+        if (Vector3.Distance(p.transform.position, this.transform.position) < 20)
+        {
+            transform.LookAt(p.transform);
+            agent.SetDestination(p.transform.position);
+        }
+        else
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= wanderTimer)
+            {
+                Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+                agent.SetDestination(newPos);
+                timer = 0;
+            }
+        }
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
     }
 }
